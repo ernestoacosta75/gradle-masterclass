a02d55b327fbfc545007fd5decda298f74a41224
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-05-07 23:46:23 +0200

    Multi-project build. Setting sub-projects dependencies

--- gradle-multiProject/gradle-multiProject-initial/allAboutNumbers-initial/plusplus-initial/src/main/java/com/denofprogramming/random/App.java
+++ gradle-multiProject/gradle-multiProject-initial/allAboutNumbers-initial/plusplus-initial/src/main/java/com/denofprogramming/random/App.java
@@ -1,24 +1,24 @@
 package com.denofprogramming.random;
 
-import org.apache.commons.math3.random.RandomDataGenerator;
+//simport org.apache.commons.math3.random.RandomDataGenerator;
 
 /**
  *
  * 
  */
-public class App {
+public class  App {
 
 	public static void main(String[] args) {
 
 		RandomGenerator aRandomGenerator = new DefaultRandomGenerator();
 		System.out.println("The number is :" + aRandomGenerator.generate());
-		System.out.println("The 2nd number is :" + generateMy());
+		//System.out.println("The 2nd number is :" + generateMy());
 
 	}
 
-	public static int generateMy() {
+/*	public static int generateMy() {
 		final RandomDataGenerator aRandomDataGenerator = new RandomDataGenerator();
 		return aRandomDataGenerator.nextInt(5, 10);
-	}
+	}*/
 
 }
